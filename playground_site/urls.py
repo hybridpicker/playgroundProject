a2c23bcbfb0cbff68a30c9026fbba5e4d6153099
Lukas Schönsgibl <lukas@schoensgibl.com>
2018-07-26 07:28:21 +0200

    templates

--- playground_site/urls.py
+++ playground_site/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import homePg.views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', homePg.views.home, name='home')
 ]
