651c4498eb59c6b57aac5d5b1df014c187abc3b9
Lukas Schönsgibl <lukas@schoensgibl.com>
2018-08-01 09:00:59 +0200

    Scroll Effect and CSS

--- homePg/models.py
+++ homePg/models.py
@@ -3,3 +3,8 @@ from django.db import models
 # Create your models here.
 
 photo = models.ImageField(upload_to="gallery")
+
+class Song(models.Model):
+    title = models.CharField(max_length=100)
+    songfile = models.FileField()
+    isPlaying = False
