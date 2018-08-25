e5670f9edf9e4e28ad8d10ec65bf655025911333
Lukas Schönsgibl <lukas@schoensgibl.com>
2018-08-25 10:10:59 +0200

    Latest changes

--- playground_site/settings.py
+++ playground_site/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
+import sys
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -76,9 +77,10 @@ WSGI_APPLICATION = 'playground_site.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-import os
 
 PROJECT_DIR = os.path.dirname(__file__)
+
+# Covers regular testing and django-coverage
 
 DATABASES = {
     'default': {
@@ -88,8 +90,11 @@ DATABASES = {
         'PASSWORD': 'Ax9kl3',
         'HOST': 'localhost',
         'PORT': '5432',
-    }
+     }
 }
+
+if 'test' in sys.argv or 'test_coverage' in sys.argv:
+    DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 
 # Password validation
