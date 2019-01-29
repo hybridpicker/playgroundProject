f1f67fe6ae198604e09b37c39e764c11a9581615
Lukas Schönsgibl <lukas@schoensgibl.com>
2019-01-29 06:27:09 +0100

    contact form

--- homePg/views.py
+++ homePg/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from song.models import Song
 from band.models import Member
 from band.models import Info
+from contact.forms import ContactForm
+from contact.email import contact_mail
 
 # Create your views here.
 
@@ -9,7 +11,26 @@ def home (request):
     songs = Song.objects
     members = Member.objects
     infos = Info.objects
-    return render(request, 'home.html', {'songs':songs, 'members':members, 'infos':infos})
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            name = request.POST['name']
+            subject = str('Anfrage: ' + name)
+            now = datetime.datetime.now()
+            today = now.date()
+            try:
+                contact_mail(from_email, subject, message)
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            render(request, 'home.html', {'songs':songs, 'members':members,
+                                                'infos':infos, 'form': form})
+    return render(request, 'home.html', {'songs':songs, 'members':members,
+                                        'infos':infos, 'form': form})
 
 def impressum (request):
     return render(request, 'impressum.html',)
